4fbf982e4500391219b844be25d702cff3e88855
Ananta Chandra Das <anantadas.dilan@gmail.com>
2017-07-18 21:33:32 +0530

    Update LoopsInScalaDemo.scala

--- src/com/ananta/demo/LoopsInScalaDemo.scala
+++ src/com/ananta/demo/LoopsInScalaDemo.scala
@@ -1,32 +1,57 @@
-
-
 package com.ananta.demo
 
 object LoopsInScalaDemo {
   def main(args: Array[String]): Unit = {
-    var i:Int = 1
-    //while loop
-    println("For while loop")
-    while(i<5){
-      println(i)
-      i=i+1
-    }
-    
-    //do-while
-    println("For do-while loop")
-    do{
-      println(i)
-      i=i-1
-    }while(i>1)
-      
-    //for loop
-      println("For loop")
-      for(i <- 1 to 5){ // val i:Int=1
-        println(i)        
+      var i:Int = 1
+     
+      //while loop
+      print("While loop : ")
+      while(i<5){
+        print(i+" ")
+        i=i+1
       }
-     println("For loop")
+      
+      //do-while
+      print("\nDo-while loop : ")
+      do{
+        print(i+" ")
+        i=i-1
+      }while(i>1)
+        
+      //for loop to to
+      print("\nFor loop to : ")
+      for(i <- 1 to 5){ // val i:Int=1
+        print(i+" ")        
+      }
+      
+      //for loop until
+      print("\nFor loop from : ")
       for(i <- 1 until 5){ // val i:Int=1
         print(i+" ")        
       }    
+      
+      //for loop filter
+      print("\nFor loop filter : ")
+      for(i<-1 to 10 if (i%2==0)){
+        print(i+" ")
+      }
+      
+      //for loop yield
+      print("\nFor loop with yield  : ")
+      var yieldResult = for(i<- 1 to 10) yield i
+      for(a<-yieldResult){
+        print(a+" ")
+      }
+      
+      //for each loop
+      print("\nFor each loop  : ")
+      var list = List(10,20,30,40,50)
+      list.foreach {(element:Int)=>print(element+" ")} 
+      
+      //for loop by
+      print("\nFor loop by  : ")
+      for(i<-1 to 10 by 2){
+        print(i+" ")
+      }
   }
 }
