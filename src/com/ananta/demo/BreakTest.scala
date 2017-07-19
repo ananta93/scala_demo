af389c35264d32193cfdfd8b144a0612235e1396
Ananta Chandra Das <anantadas.dilan@gmail.com>
2017-07-19 22:53:49 +0530

    Break and function in scala test

---
+++ src/com/ananta/demo/BreakTest.scala
@@ -0,0 +1,15 @@
+package com.ananta.demo
+import scala.util.control.Breaks._  // importing package
+
+object BreakTest {
+  def main(args: Array[String]): Unit = {
+    breakable {                  // Breakable method to avoid exception  
+      for (i <- 1 to 10) {
+        if (i == 5)
+          break                 // use of break
+        else
+          println(i)
+      }
+    }
+  }
+}
---
+++ src/com/ananta/demo/FunctionTest.scala
@@ -0,0 +1,43 @@
+package com.ananta.demo
+
+object FunctionTest {
+  def functionWithoutParam(){
+    println("Function Without paramater called")
+  }
+  def functionWithParam(a:Int){
+    println("Function with parameter called : "+a)
+  }
+  def functionWithAssignment()={
+    print("Function with assignment : ")
+    var b = 20
+    b
+  }
+  
+  //Function with recursion
+  def recursiveFunction(c:Int):Int={
+    if (c==0)
+      1
+    else
+      c*(recursiveFunction(c-1))  
+  }
+  
+  //default argument
+  def functionDefaultArgument(m:Int, n:Int=10):Int={ // default argument should be assigned to parameter at the end
+   (m+n)
+  }
+  //Named argument
+  def functionNamedArgument(lName:String,fName:String){
+    println("Full name from Named parameter : "+fName+" "+lName)
+  }
+  def main(args: Array[String]): Unit = {
+    functionWithoutParam()
+    functionWithParam(10)
+    var resultFromFunction = functionWithAssignment()
+    println(resultFromFunction)
+    var resultFromRecursive=recursiveFunction(5)
+    println("Factorial from recursive function : "+resultFromRecursive)
+    var resultDefault=functionDefaultArgument(20)
+    println("Addition from default argument : "+resultDefault)
+    functionNamedArgument(fName="Ananta", lName="Das")
+  }
+}
